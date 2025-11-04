013d81ac7b6d048c7d93dbd974c357156896a79a
Surasith <surasith.kae@ascendcorp.com>
2025-11-04 23:22:57 +0700

    fix: get input

--- src/action.ts
+++ src/action.ts
@@ -6,11 +6,13 @@ export function setupAction() {
   const ghToken = getInput("github-token");
   const repo =
     getInput("repository") || context.repo.owner + "/" + context.repo.repo;
-  const baseRef = getInput("base-ref");
-  const headRef = getInput("head-ref");
-  const fileToCompare = getInput("env-file-path");
+  const baseRef = getInput("base-ref") || "latest";
+  const headRef =
+    getInput("head-ref") || context.ref.replace("refs/heads/", "");
+  const fileToCompare = getInput("env-file-path") || ".env.example";
 
   if (!repo || !baseRef || !headRef || !fileToCompare) {
+    console.table({ repo, baseRef, headRef, fileToCompare });
     throw new Error(ERRORS.invalidArgs);
   }
 
